1a10c6ffe04152752fd6f07a056dccad1a54b084
randomdude786 <khanadiluddin@gmail.com>
2025-10-29 20:59:56 +0530

    Test - add failing test for custom delimiter syntax (//;/n1;2)

--- src/test/java/testDrivenDevelopment/StringCalculatorTest.java
+++ src/test/java/testDrivenDevelopment/StringCalculatorTest.java
@@ -26,6 +26,10 @@ public class StringCalculatorTest {
 	public void testNewlineAsDelimiter() {
 	    assertEquals(6, StringCalculator.add("1\n2,3"));
 	}
-	
+	@Test
+	public void testCustomDelimiterSyntax() {
+	    assertEquals(3, StringCalculator.add("//;\n1;2"));
+	}
+
 
 }
